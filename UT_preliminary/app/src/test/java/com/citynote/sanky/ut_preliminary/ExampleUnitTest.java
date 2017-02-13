20d8c4c0dd1646bf6987ea48c3ce7e4cb1e29489
sanket patel <shpatel8112@gmail.com>
2017-02-13 17:24:06 +0530

    13.2.2017 startapp davelopment

---
+++ UT_preliminary/app/src/test/java/com/citynote/sanky/ut_preliminary/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.citynote.sanky.ut_preliminary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
